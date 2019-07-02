6870ddaf5670e8c76b2343a0080ed81d12eb0c22
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-02 20:48:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.alphabetical
     @@all.sort_by! { |song| song.name}
    end 
-  def self.new_from_filename(new_file)
+  def self.new_from_filename(file_name)
     new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
     new_song.artist_name = file_name.split(" - ")[0]
