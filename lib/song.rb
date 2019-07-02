c56e1ce465393f09191ee3183403396c1263ce47
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-02 20:50:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,10 @@ class Song
     new_song
   end 
   def self.create_from_filename(artist_name)
-    
+     new_song = self.new
+    new_song.name = artist_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = artist_name.split(" - ")[0]
+    @@all << new_song
   end 
   def self.destroy_all
     @@all.clear
